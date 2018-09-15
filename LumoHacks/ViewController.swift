9a7571b8bf42d0c04a4b5b30b2e6df47f92cf03b
flo <xxflo.h13@gmail.com>
2018-09-15 10:54:44 -0700

    Initial Commit

---
+++ LumoHacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LumoHacks
+//
+//  Created by flo. on 2018-09-15.
+//  Copyright © 2018 flo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
